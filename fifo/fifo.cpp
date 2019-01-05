4ef29a0a2226102e6204a0bfce0742d7944edf51
Cloud User <hu_peng@yahoo.com>
2019-01-05 02:39:02 +0000

    Correctly print out element being de-queued.

--- fifo/fifo.cpp
+++ fifo/fifo.cpp
@@ -34,11 +34,11 @@ int MyFIFO::deQueue()
 {
     if ( back == -1 ) 
         return 1;
+    cout << fifoQ[0] << " deQueued" << endl;
     for ( int i = 0; i < back; i++ )
     {
         fifoQ[i] = fifoQ[i+1];
     }; 
-    cout << fifoQ[back] << " deQueued" << endl;
     back--;
     return 0;
 };
