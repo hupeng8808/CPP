a13ef742ffef02cbbbfc4f6a4def6b497834816e
Cloud User <hu_peng@yahoo.com>
2019-01-05 02:41:42 +0000

    add indentation for de-queue message for better readability.

--- fifo/fifo.cpp
+++ fifo/fifo.cpp
@@ -34,7 +34,7 @@ int MyFIFO::deQueue()
 {
     if ( back == -1 ) 
         return 1;
-    cout << fifoQ[0] << " deQueued" << endl;
+    cout << "    " << fifoQ[0] << " deQueued" << endl;
     for ( int i = 0; i < back; i++ )
     {
         fifoQ[i] = fifoQ[i+1];
